f01237a8f740744a34bbe8e16c92e905217b9830
Kevin Doyle <kjdoyle@ca.ibm.com>
2020-06-10 21:40:46 -0400

    Add metadata.yml file for easy notebook addition and update build scripts to handle downloading Sparkling Water automatically

--- build_package.sh
+++ build_package.sh
@@ -3,6 +3,8 @@
 PACKAGE_NAME="H2O_Sparklingwater"
 PACKAGE_VERSION="3.28.0.3-1-2.4"
 BUILD_DIR=${BUILD_DIR:-./build}
+DOWNLOAD_URL="https://s3.amazonaws.com/h2o-release/sparkling-water/spark-2.4/3.28.0.3-1-2.4/sparkling-water-3.28.0.3-1-2.4.zip"
+
 
 if [ -d "$BUILD_DIR" ]; then
     rm -fr "$BUILD_DIR"
@@ -13,5 +15,6 @@ cp -f deployment.xml $BUILD_DIR
 cp -fr scripts $BUILD_DIR
 cp -fr package $BUILD_DIR
 
-cd $BUILD_DIR
+cd package;curl ${DOWNLOAD_URL} --output sparkling-water-${PACKAGE_VERSION}.zip
+cd ../$BUILD_DIR
 tar czvf "${PACKAGE_NAME}-${PACKAGE_VERSION}.tar.gz" deployment.xml scripts package
